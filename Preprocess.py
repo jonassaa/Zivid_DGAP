031e3c805a12e04460cea13f0f5f1e89e0c637ce
Jonas Strand Aasberg <jonassaa@stud.ntnu.no>
2022-02-23 14:28:18 +0100

    Added support for subsampling and exclusion of color

--- Preprocess.py
+++ Preprocess.py
@@ -5,12 +5,16 @@ import os
 import zivid
 import shutil
 from datetime import date
+import time
 import random
 
 
-def readZividPCD(framefile):
+def readZividPCD(framefile,subsample):
     frame = zivid.frame.Frame(framefile)
     pcd = frame.point_cloud()
+
+    if subsample is not None:
+        pcd = pcd.downsample(subsample)
     
     xyz = pcd.copy_data("xyz")
     rgba = pcd.copy_data("rgba")
@@ -19,21 +23,32 @@ def readZividPCD(framefile):
     return xyz, rgba, normals
 
 
-def saveZividPcdAsNpz(savedir,savename,framefile,normVectorSave = False):
+def saveZividPcdAsNpz(savedir,savename,framefile,normVectorSave = False, save_color=False, subsample = None):
     if not os.path.exists(savedir):
         os.mkdir(savedir)
 
-    xyz,rgba,normals = readZividPCD(framefile=framefile)
+    xyz,rgba,normals = readZividPCD(framefile=framefile,subsample=subsample)
 
-    if  normVectorSave:
+    if  normVectorSave and save_color:
         np.savez_compressed(os.path.join(savedir,savename),
-                            rgb = rgba[:,:,0:3].reshape(1920*1200,3),
-                            pcd = xyz.reshape(1920*1200,3),
+                            rgb = rgba[:,:,0:3].reshape(np.shape(xyz)[0]*np.shape(xyz)[1],3),
+                            pcd = xyz.reshape(np.shape(xyz)[0]*np.shape(xyz)[1],3),
                             normals = normals)
-    else:
+    
+    elif  normVectorSave and not save_color:
         np.savez_compressed(os.path.join(savedir,savename),
-                            rgb = rgba[:,:,0:3].reshape(1920*1200,3),
-                            pcd = xyz.reshape(1920*1200,3))
+                            pcd = xyz.reshape(np.shape(xyz)[0]*np.shape(xyz)[1],3),
+                            normals = normals)
+
+    elif  not normVectorSave and save_color:
+        np.savez_compressed(os.path.join(savedir,savename),
+                            rgb = rgba[:,:,0:3].reshape(np.shape(xyz)[0]*np.shape(xyz)[1],3),
+                            pcd = xyz.reshape(np.shape(xyz)[0]*np.shape(xyz)[1],3))
+
+
+    elif   not normVectorSave and not save_color:
+        np.savez_compressed(os.path.join(savedir,savename),
+                            pcd = xyz.reshape(np.shape(xyz)[0]*np.shape(xyz)[1],3))
 
 
 def testZividSaveLoad():
@@ -46,26 +61,24 @@ def testZividSaveLoad():
     print(loaded["xyz"][200,200])
     print(loaded["normals"][200,200])
     
-def splitTrainVal(allFile,trainFile,valFile,valFraction = 0.1):
-    f = open(allFile,"r")
-    allScans = f.read().split("\n")
-    print(allScans[-1])
-    del allScans[-1]
-    f.close()
+def splitTrainVal(objectList,trainFile,valFile,valFraction = 0.1):
 
-    trainN = int(len(allScans)*1-valFraction)
-    valN = len(allScans)-trainN
+    trainN = int(len(objectList)*1-valFraction)
 
-    trainList = random.sample(allScans,trainN)
-    valList = random.sample(allScans,valN)
+    trainList = random.sample(objectList,trainN)
+    valList = []
+
+    for n in objectList:
+        if not trainList.__contains__(n):
+            valList.append(n)
 
 
-    f = open(trainFile,"w+")
+    f = open(trainFile,"a")
     for t in trainList:
         f.write(f"{t}\n")
     f.close()
 
-    f = open(valFile,"w+")
+    f = open(valFile,"a")
     for t in valList:
         f.write(f"{t}\n")
     f.close()
@@ -78,9 +91,12 @@ def main():
     parser.add_argument("--zivid_camera_file",help="Path to .zfc camera for camera emulation", type=str, default="./FileCameraZividOne.zfc")
     parser.add_argument("--val_fraction", type=float, default=0.1)
     parser.add_argument("--include_normals",type=bool, default=False)
+    parser.add_argument("--include_color",type= bool, default=False)
+    parser.add_argument("--subsample", type=str, default = None, help="Default is no subsamplng, ommit this flag for raw data input. For subsampling input arguments by4x4 by3x3 or by2x2 ")
     parser.add_argument("--dataset_output_name",type=str,default="ZividDataset")
-    #parser.add_argument("--include_normals")
-    #parser.add_argument("--include_normals")
+    parser.add_argument("--train_file_name",type= str, default="trainZivid.txt")
+    parser.add_argument("--val_file_name",type= str, default="valZivid.txt")
+
     args = parser.parse_args()    
 
     # Connecting ZividFileCamera
@@ -101,7 +117,11 @@ def main():
     for (dirpath, dirnames, filenames) in walk(datasetFolder):
         directories.extend(dirnames)
 
+    if os.path.exists("./savefolder"):
+        shutil.rmtree("./savefolder")
     os.makedirs("./savefolder")
+
+    splitTrainVal(directories,os.path.join("./savefolder",args.train_file_name), os.path.join("./savefolder",args.val_file_name), args.val_fraction)
 
     for dir in directories:
         print(f"\nNow processing files in {os.path.join(datasetFolder,dir)}")
@@ -110,7 +130,7 @@ def main():
         f = open(os.path.join("./savefolder",str(dir)+"_all.txt"),"x")
         for file in files:
             print(file)
-            saveZividPcdAsNpz("./saveFolder",f"{dir}_{i}",os.path.join(datasetFolder, dir, file),normVectorSave=args.include_normals)
+            saveZividPcdAsNpz("./saveFolder",f"{dir}_{i}",os.path.join(datasetFolder, dir, file),normVectorSave=args.include_normals, save_color=args.include_color,subsample=args.subsample)
             f.write(f"{dir}_{i}\n")
             i += 1
         f.close()
@@ -141,7 +161,10 @@ def main():
             listFile.close()
             pairFile.close()
 
-    shutil.make_archive(f"{args.dataset_output_name}_{str(date.today())}",'zip',"./saveFolder")
+    t = time.localtime()
+    current_time = time.strftime("%H%M", t)
+
+    shutil.make_archive(f"{args.dataset_output_name}_{str(date.today())}_{str(current_time)}",'zip',"./saveFolder")
     shutil.rmtree("./saveFolder")
 
 
